2fdc49957c145f879a776c0b444b0ded0879a949
samuel <samuel.antonio@totvs.com.br>
2019-12-12 16:27:29 -0300

    test service using postgres

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(express.urlencoded({ extended: false }));
+const { User } = require('./models');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/register', async (req, res) => {
+  const user = await User.create(req.body);
+  res.json(user);
+});
+
+app.get('/find/:id', async (req, res) => {
+    
+  res.json(await User.findAll());
+});
+
+app.get('/findall', async (req, res) => {
+    res.json(await User.findAll());
+});
+
+app.put('/update/:id', (req, res) => {
+  res.json('Im in update');
+});
+
+app.delete('/delete/:id', (req, res) => {
+  res.json('Im in delete');
 });
 
 app.listen(3000);
